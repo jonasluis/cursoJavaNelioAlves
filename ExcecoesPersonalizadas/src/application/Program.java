a27bbb112bd45dd068343554172ab01b3effe6d5
Jonas Luis <jonasluis66@gmail.com>
2024-04-11 16:57:38 -0300

    Bad solution. tratando excecoes com String .

--- ExcecoesPersonalizadas/src/application/Program.java
+++ ExcecoesPersonalizadas/src/application/Program.java
@@ -15,23 +15,22 @@ public class Program {
 
 		Scanner sc = new Scanner(System.in);
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		
+
 		List<Reservation> list = new ArrayList<>();
-		
+
 		System.out.print("Room number: ");
 		int roomNumber = sc.nextInt();
 		System.out.print("Check-in date (dd/MM/yyyy): ");
 		Date checkin = sdf.parse(sc.next());
 		System.out.print("Check-out date (dd/MM/yyyy): ");
 		Date checkout = sdf.parse(sc.next());
-		
+
 		if (!checkout.after(checkin)) {
 			System.out.println("Error in reservation: Check-out date must be after check-in date");
-		}
-		else {
+		} else {
 			Reservation reservation = new Reservation(roomNumber, checkin, checkout);
 			System.out.println(reservation);
-			
+
 			System.out.println();
 			System.out.println("Enter data to update the reservation:");
 			System.out.print("Check-in date (dd/MM/yyyy): ");
@@ -39,21 +38,15 @@ public class Program {
 			System.out.print("Check-out date (dd/MM/yyyy): ");
 			checkout = sdf.parse(sc.next());
 			
-			Date now = new Date();
-			if (checkin.before(now) || checkout.before(now)) {
-				System.out.println("Error in reservation: Reservation dates for update must be future dates");
-			} else if (!checkout.after(checkin)) {
-				System.out.println("Error in reservation: Check-out date must be after check-in date");
-			}else {
-				reservation.updateDates(checkin, checkout);
+			String error = reservation.updateDates(checkin, checkout);
+			if (error != null) {
+				System.out.println("Error in reservation: " + error);
+			} else {
 				System.out.println(reservation);
 			}
-			
-			
 		}
 		
-		
-		
+
 		sc.close();
 	}
 
--- ExcecoesPersonalizadas/src/model/entities/Reservation.java
+++ ExcecoesPersonalizadas/src/model/entities/Reservation.java
@@ -9,11 +9,11 @@ public class Reservation {
 	private Integer roomNumber;
 	private Date checkin;
 	private Date checkout;
-	
+
 	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-	
+
 	public Reservation() {
-		
+
 	}
 
 	public Reservation(Integer roomNumber, Date checkin, Date checkout) {
@@ -42,19 +42,24 @@ public class Reservation {
 		long diff = checkout.getTime() - checkin.getTime();
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 	}
-	
-	public void updateDates(Date checkin, Date checkout) {
+
+	public String updateDates(Date checkin, Date checkout) {
+		Date now = new Date();
+		if (checkin.before(now) || checkout.before(now)) {
+			return "Reservation dates for update must be future dates";
+		}
+		if (!checkout.after(checkin)) {
+			return "Check-out date must be after check-in date";
+		}
 		this.checkin = checkin;
 		this.checkout = checkout;
+		return null;
 	}
 
 	@Override
 	public String toString() {
-		return "Reservation: Room " + roomNumber 
-				+ ", checkin: "	+ sdf.format(checkin) 
-				+ ", check-out: " + sdf.format(checkout) + ", "
-				+ duration() + " nights";
+		return "Reservation: Room " + roomNumber + ", checkin: " + sdf.format(checkin) + ", check-out: "
+				+ sdf.format(checkout) + ", " + duration() + " nights";
 	}
-	
-	
+
 }
