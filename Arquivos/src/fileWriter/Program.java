b4996ce6d255a64c2796e30273faf67eddbfa42a
Jonas Luis <jonasluis66@gmail.com>
2024-04-23 16:59:03 -0300

    Aula de Arquivos e exercicio

---
+++ Arquivos/src/fileWriter/Program.java
@@ -0,0 +1,27 @@
+package fileWriter;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		String[] lines = new String[] { "Good morning", "Good afternoon", "Good night" };
+
+		String path = "C:\\Windows\\Temp\\out.txt";
+
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(path))) {
+			//(new FileWriter(path, true))) = acrescentar & (new FileWriter(path))) = recirar
+			
+			for (String line : lines) {
+				bw.write(line);
+				bw.newLine();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
