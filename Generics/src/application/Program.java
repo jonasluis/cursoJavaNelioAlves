745926f18ca8cbcce71e7c7eadd3e50e045ff385
Jonas Luis <jonasluis66@gmail.com>
2024-04-29 12:01:48 -0300

    Solução com generics

--- Generics/src/application/Program.java
+++ Generics/src/application/Program.java
@@ -10,19 +10,18 @@ public class Program {
 
 		Scanner sc = new Scanner(System.in);
 		
-		PrintService ps = new PrintService();
+		PrintService<Integer> ps = new PrintService<>();
 		
 		System.out.print("How many values? ");
 		int n = sc.nextInt();
 		
-		ps.addValue("Maria");
 		for (int i = 0; i < n; i++) {
 			int value = sc.nextInt();
 			ps.addValue(value);
 		}
 		
 		ps.print();
-		Integer x = (Integer) ps.first();
+		Integer x = ps.first();
 		System.out.println("First: " + x);
 		
 		sc.close();
